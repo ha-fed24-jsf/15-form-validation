a9d1ef203e5462bea0646e81b5d93f3fdba8c937
David Andersson <lejonmanen@gmail.com>
2025-04-09 11:49:27 +0200

    Complete validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -18,18 +18,23 @@ const Form = () => {
 	const [data, setData] = useState({ name: '', email: '' })
 	const [touched, setTouched] = useState({ name: false, email: false })
 
+	// Utmaning: det går att flytta det mesta av valideringslogiken till en separat funktion i en annan fil
+	// function validateForm(data, touched) --> return { css, message, formIsValid }
+
 	let css = {
 		name: '',
 		email: ''
 	}
 	if( touched.name ) css.name = 'valid'
+	if( touched.email ) css.email = 'valid'
+	// Pro-metod: använd Object.keys(touched).forEach i stället
 
 	let message = {
 		name: '',
 		email: ''
 	}
 	const results = schema.validate(data)
-	console.log('Form: validation results: ', results)
+	// console.log('Form: validation results: ', results)
 	// if( validation fails ) css.name = 'invalid'
 	if( results.error ) {
 		results.error.details.forEach(e => {
@@ -51,8 +56,16 @@ const Form = () => {
 				// det enda som kan bli fel, är om namn-fältet är tomt
 				message.name = 'Skriv ditt namn.'
 			}
+			else if( e.context.key === 'email' ) {
+				if( data.email === '' ) {
+					message.email = 'Skriv din e-postadress.'
+				} else {
+					message.email = 'Skriv adressen på formatet "namn@domän.se".'
+				}
+			}
 		})
 	}
+	const formIsValid = !results.error
 
 	return (
 		<div className="form">
@@ -68,13 +81,18 @@ const Form = () => {
 				<p className="error"> {message.name} </p>
 
 				<label> E-post </label>
-				<input />
-				<p className="error"></p>
+				<input
+					className={css.email}
+					value={data.email}
+					onChange={event => setData({ ...data, email: event.target.value })}
+					onBlur={() => setTouched({ ...touched, email: true })}
+					/>
+				<p className="error"> {message.email} </p>
 
 			</section>
 
 			<section className="form-field">
-				<button> Spara </button>
+				<button disabled={!formIsValid}> Spara </button>
 			</section>
 		</div>
 	)
