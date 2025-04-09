8c52a05cf4071f88ee2dd7b7166f3441dd5c117d
David Andersson <lejonmanen@gmail.com>
2025-04-09 10:23:55 +0200

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className="app">
+			<header>
+				<h1> Formulärvalidering </h1>
+			</header>
+			<main>
+				Formulär här
+			</main>
+		</div>
+	)
+}
+
+export default App
